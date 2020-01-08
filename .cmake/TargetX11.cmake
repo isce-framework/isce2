aa140d5f799dfe1036a7b7e366dcde9cfd374eae
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-01-08 12:08:23 -0800

    Fix X11 lib detection

--- .cmake/TargetX11.cmake
+++ .cmake/TargetX11.cmake
@@ -2,12 +2,15 @@ find_package(X11)
 
 if(X11_FOUND)
 
+    set(X11_X11_FOUND TRUE)
+
     foreach(component
+            X11
             Xmu
             Xt
             )
 
-        if(X11_${Xmu}_FOUND AND NOT TARGET X11::${component})
+        if(X11_${component}_FOUND AND NOT TARGET X11::${component})
             add_library(X11::${component} IMPORTED INTERFACE)
             target_include_directories(X11::${component} SYSTEM
                 INTERFACE ${X11_${component}_INCLUDE_PATH})
