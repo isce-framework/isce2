e84b298d0c457305a9c3d3669db1eb925decff8f
Simon Kraatz <49723397+sgk0@users.noreply.github.com>
2020-05-03 13:50:03 -0400

    Add files via upload

--- applications/rtcApp.py
+++ applications/rtcApp.py
@@ -107,6 +107,28 @@ GEOCODE_BOX = Application.Parameter(
     doc='Bounding box for geocoding - South, North, West, East in degrees'
                                     )
 
+EPSG = Application.Parameter(
+    'epsg',
+    public_name='epsg id',
+    default = '',
+    type=str,
+    doc='epsg code for roi'
+                                    )
+
+GSPACING = Application.Parameter('gspacing',
+            public_name='geocode spacing',
+            default = 100.0,
+            type = float,
+            doc = 'Desired grid spacing of geocoded product in meters, in the specified UTM grid.'
+                                    ) 
+
+INTMETHOD = Application.Parameter('intmethod',
+            public_name='geocode interpolation method',
+            default = 'bilinear',
+            type = str,
+            doc = 'Desired grid spacing of geocoded product in meters, in the specified UTM grid.'
+                                    ) 
+
 PICKLE_DUMPER_DIR = Application.Parameter(
     'pickleDumpDir',
     public_name='pickle dump directory',
@@ -155,11 +177,11 @@ NUMBER_RANGE_LOOKS = Application.Parameter('numberRangeLooks',
 )
 
 POSTING = Application.Parameter('posting',
-            public_name='azimuth looks',
-            default = 20.0,
+            public_name='posting',
+            default = 10.0,
             type = float,
             mandatory = False,
-            doc = 'Posting of data used to determine looks') 
+            doc = 'Posting of data. This can be any integer multiple of the product resolution. Used to determine looks') 
 
 POLARIZATIONS = Application.Parameter('polarizations',
             public_name='polarizations',
@@ -222,6 +244,9 @@ class GRDSAR(Application):
                       NUMBER_RANGE_LOOKS,
                       POSTING,
                       GEOCODE_BOX,
+                      EPSG,
+                      GSPACING,
+                      INTMETHOD,
                       PICKLE_DUMPER_DIR,
                       PICKLE_LOAD_DIR,
                       RENDERER,
@@ -363,7 +388,8 @@ class GRDSAR(Application):
         self.verifyDEM = RtcProc.createVerifyDEM(self)
         self.multilook = RtcProc.createLooks(self)
         self.runTopo  = RtcProc.createTopo(self)
-#        self.runGeocode = RtcProc.createGeocode(self)
+        self.runNormalize = RtcProc.createNormalize(self)
+        self.runGeocode = RtcProc.createGeocode(self)
 
         return None
 
@@ -392,9 +418,12 @@ class GRDSAR(Application):
         ##Run topo for each bursts
         self.step('topo', func=self.runTopo)
 
+	    ##Run normalize to get gamma0
+        self.step('normalize', func=self.runNormalize)
+
         # Geocode
-#        self.step('geocode', func=self.runGeocode,
-#                args=(self.geocode_list, self.do_unwrap, self.geocode_bbox))
+        self.step('geocode', func=self.runGeocode,
+                args=(self.geocode_list, self.geocode_bbox))
 
         return None
 
@@ -416,13 +445,12 @@ class GRDSAR(Application):
 
         ##Run topo for each burst
         self.runTopo()
-
-        ###Compute covariance
-#        self.runEstimateCovariance()
+	
+	##Run normalize to get gamma0
+        self.runNormalize()
 
         # Geocode
-#        self.runGeocode(self.geocode_list, self.do_unwrap, self.geocode_bbox)
-
+        self.runGeocode()
 
         timeEnd = time.time()
         logger.info("Total Time: %i seconds" %(timeEnd - timeStart))
