708593b322cbed3cdcd6d47202323472dcfaa57b
CunrenLiang <56097947+CunrenLiang@users.noreply.github.com>
2020-05-13 09:54:54 -0700

    fixed a bug in radar dem matching

--- components/isceobj/Alos2Proc/runRdrDemOffset.py
+++ components/isceobj/Alos2Proc/runRdrDemOffset.py
@@ -99,6 +99,8 @@ def runRdrDemOffset(self):
         self._insar.radarDemAffineTransform = [1.0, 0.0, 0.0, 1.0, 0.0, 0.0]
         catalog.addItem('warning message', 'land area too small for estimating offsets between radar and dem', 'runRdrDemOffset')
 
+        os.chdir('../../')
+
         catalog.printToLog(logger, "runRdrDemOffset")
         self._insar.procDoc.addAllFromCatalog(catalog)
 
