f09213eb33d047859dc11a0b1381ba4f7201360b
Gerald Manipon <pymonger@gmail.com>
2020-07-16 16:08:57 -0700

    various updates from ARIA-related projects (#149)
    * add Dockerfile and SConfigISCE to build cuda-enabled modules
    
    * add Dockerfile and SConfigISCE to build cuda-enabled modules
    
    * set correct name
    
    * update for centos7 and cuda-dev image
    
    * restore
    
    * disable GPUampcor
    
    * update
    
    * save
    
    * sync up Dockerfile updates
    
    * fix base image
    
    * change docker image names
    
    * create gpu vs. cpu specific tags
    
    * update with nodata WBD Stitcher
    
    * fix segmentation fault when running sciflo workflows
    
    - remove dependencies that downgrade the `geos` conda package
    
    * sync fix for segmentation fault to cuda builds
    
    * ping ffi to version 1.12.2
    
    Latest version of ffi, 1.13.0, breaks installation of fpm:
    ```
    $ sudo gem install --no-ri --no-rdoc fpm
    Fetching: cabin-0.9.0.gem (100%)
    Successfully installed cabin-0.9.0
    Fetching: backports-3.17.2.gem (100%)
    Successfully installed backports-3.17.2
    Fetching: arr-pm-0.0.10.gem (100%)
    Successfully installed arr-pm-0.0.10
    Fetching: clamp-1.0.1.gem (100%)
    Successfully installed clamp-1.0.1
    Fetching: ffi-1.13.0.gem (100%)
    ERROR:  Error installing fpm:
            ffi requires Ruby version >= 2.3.
    ```
    TODO: remove pin when ffi's backwards compatiblity is restored.
    
    * comment out unused import
    
    * add build circleci job to PR tests
    
    * pin ffi in cuda build
    
    * revert logging hackery
    
    * fix "target_include_directories called with non-compilable target type" errors
    
    * Keep source directory for topsStack
    
    Co-authored-by: dustinlo <dustin.k.lo@jpl.nasa.gov>
    Co-authored-by: shitong01 <stchin@ntu.edu.sg>
    Co-authored-by: Ryan Burns <47790121+rtburns-jpl@users.noreply.github.com>

--- docker/build_env.sh
+++ docker/build_env.sh
@@ -1,5 +1,5 @@
-export BUILD_DIR=/root/build-isce
-export ISCE_SRC=$HOME/isce2
+export ISCE_INSTALL_ROOT=/opt/isce2
+export ISCE_SRC=$ISCE_INSTALL_ROOT/src/isce2
+export BUILD_DIR=$ISCE_INSTALL_ROOT/build-isce
 export SCONS_CONFIG_DIR=$ISCE_SRC/configuration
 export ISCE_BUILD_ROOT=$BUILD_DIR/build
-export ISCE_INSTALL_ROOT=/opt/isce2
