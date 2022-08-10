052f93eddd1d08c7cb62b666d15830e3deec31ca
Eric Fielding <EJFielding@users.noreply.github.com>
2022-08-10 14:56:00 -0700

    Update release_history.py
    removed one extra whitespace

--- release_history.py
+++ release_history.py
@@ -2,7 +2,7 @@
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Copyright 2013 California Institute of Technology. ALL RIGHTS RESERVED.
-#  
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
