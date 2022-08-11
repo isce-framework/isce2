1e60308201ededa5aac9f620c1237e1f8effb4ea
Eric Fielding <EJFielding@users.noreply.github.com>
2022-08-10 20:48:04 -0700

    More https changes in isceobj iscesys alos2proc
    Changed other files with hard-coded SRTM or SWBD URL to https

--- components/iscesys/DataManager/Dem3Manager.py
+++ components/iscesys/DataManager/Dem3Manager.py
@@ -2,19 +2,19 @@
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Copyright 2012 California Institute of Technology. ALL RIGHTS RESERVED.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # United States Government Sponsorship acknowledged. This software is subject to
 # U.S. export control laws and regulations and has been classified as 'EAR99 NLR'
 # (No [Export] License Required except when exporting to an embargoed country,
@@ -43,10 +43,10 @@ EXTRA = Component.Parameter('_extra',
           +' otherwise if no value is provided is then interpreted as None by the xml reader.')
 
 URL = Component.Parameter('_url',
-    public_name = 'URL',default = 'http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL3.003/2000.02.11',
+    public_name = 'URL',default = 'https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL3.003/2000.02.11',
     type = str,
     mandatory = False,
-    doc = "Url for the high resolution DEM.") 
+    doc = "Url for the high resolution DEM.")
 
 TILE_SIZE = Component.Parameter('_tileSize',
     public_name = 'tileSize',
@@ -64,8 +64,8 @@ class Dem3Manager(Dem1Manager):
                        URL,
                        TILE_SIZE
                        ) + Dem1Manager.parameter_list
-        
-    
+
+
     def __init__(self,family = '', name = ''):
         self.parameter_list = self.parameter_list + super(Dem1Manager,self).parameter_list
         self.updateParameters()
@@ -74,4 +74,3 @@ class Dem3Manager(Dem1Manager):
     def updateParameters(self):
         self.extendParameterList(Dem1Manager,Dem3Manager)
         super(Dem3Manager,self).updateParameters()
-   
