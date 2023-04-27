c50cc112297ef2acab2e59077362a0f253739069
Yujie Zheng <zyjatpku@gmail.com>
2023-04-27 10:57:18 -0700

    Add geolocation constraint option

--- contrib/stack/stripmapStack/unpackFrame_ERS_ENV.py
+++ contrib/stack/stripmapStack/unpackFrame_ERS_ENV.py
@@ -20,6 +20,8 @@ def cmdLineParse():
     parser = argparse.ArgumentParser(description='Unpack ERS(ESA) SLC data and store metadata in pickle file.')
     parser.add_argument('-i','--input', dest='datadir', type=str,
             required=True, help='Input ERS files path')
+    parser.add_argument('-b', '--box' ,dest='bbox', type=float, nargs=4, default=None,
+            help='Bbox (SNWE in degrees)')
     parser.add_argument('-o', '--output', dest='slcdir', type=str,
             required=True, help='Output SLC directory')
     parser.add_argument('--orbitdir', dest='orbitdir', type=str,required=True, help='Orbit directory')
@@ -59,28 +61,30 @@ def unpack(fname, slcname, orbitdir):
     obj.output = os.path.join(slcname,os.path.basename(slcname)+'.slc')
     obj.extractImage()
     obj.frame.getImage().renderHdr()
+    obj.extractDoppler()
 
-    ####  computation of "poly" adapted from line 339 - line 353 of  Components/isceobj/Sensor/ERS_EnviSAT_SLC.py ###
-    coeffs = obj.dopplerRangeTime
-    dr = obj.frame.getInstrument().getRangePixelSize()
-    rref = 0.5 * Const.c * obj.rangeRefTime
-    r0 = obj.frame.getStartingRange()
-    norm = 0.5 * Const.c / dr
+    # ####  computation of "poly" adapted from line 339 - line 353 of  Components/isceobj/Sensor/ERS_EnviSAT_SLC.py ###
+    #######  removed this section and added  obj.extractDoppler() above instead. Doesn't seem to change anything in the processing. The latter is required for cropFrame.
+    # coeffs = obj.dopplerRangeTime
+    # dr = obj.frame.getInstrument().getRangePixelSize()
+    # rref = 0.5 * Const.c * obj.rangeRefTime
+    # r0 = obj.frame.getStartingRange()
+    # norm = 0.5 * Const.c / dr
 
-    dcoeffs = []
-    for ind, val in enumerate(coeffs):
-        dcoeffs.append( val / (norm**ind))
+    # dcoeffs = []
+    # for ind, val in enumerate(coeffs):
+    #     dcoeffs.append( val / (norm**ind))
     
-    poly = Poly1D.Poly1D()
-    poly.initPoly(order=len(coeffs)-1)
-    poly.setMean( (rref - r0)/dr - 1.0)
-    poly.setCoeffs(dcoeffs)
+    # poly = Poly1D.Poly1D()
+    # poly.initPoly(order=len(coeffs)-1)
+    # poly.setMean( (rref - r0)/dr - 1.0)
+    # poly.setCoeffs(dcoeffs)
 
 
     pickName = os.path.join(slcname, 'data')
     with shelve.open(pickName) as db:
         db['frame'] = obj.frame
-        db['doppler'] = poly 
+        # db['doppler'] = poly 
 
 
 if __name__ == '__main__':
@@ -105,3 +109,11 @@ if __name__ == '__main__':
 
         shelveFile = os.path.join(slcname, 'data')
         write_xml(shelveFile,slcFile)
+
+        if inps.bbox is not None:
+            slccropname = os.path.abspath(os.path.join(inps.slcdir+'_crop',date))
+            os.makedirs(slccropname, exist_ok=True)
+            cmd = 'cropFrame.py -i {} -o {} -b {}'.format(slcname, slccropname, ' '.join([str(x) for x in inps.bbox]))
+            print(cmd)
+            os.system(cmd)
+    
