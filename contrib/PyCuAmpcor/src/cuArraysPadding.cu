85c8ec1e3d5663b48de1800679bfe91bd23c6434
Lijun Zhu <ljzhu@gps.caltech.edu>
2024-12-12 20:33:16 -0800

    PyCuAmpcor: use split spectrum for even sequence oversampling

--- contrib/PyCuAmpcor/src/cuArraysPadding.cu
+++ contrib/PyCuAmpcor/src/cuArraysPadding.cu
@@ -9,28 +9,78 @@
 
 // cuda kernel for zero padding for FFT oversampling,
 // for both even and odd sequences
+// @param[in] image1 input images
+// @param[inout] image2 output images - memset to 0 in prior
 __global__ void cuArraysPaddingMany_kernel(
     const float2 *image1, const int height1, const int width1, const int size1,
     float2 *image2, const int height2, const int width2, const int size2, const float factor )
 {
-
-    int tx = threadIdx.x + blockDim.x*blockIdx.x;
-    int ty = threadIdx.y + blockDim.y*blockIdx.y;
+    // thread indices are for input image1
+    int x1 = threadIdx.x + blockDim.x*blockIdx.x;
+    int y1 = threadIdx.y + blockDim.y*blockIdx.y;
     int imageIdx =  blockIdx.z;
 
-    //  make sure threads are in range
-    if(tx < height1 && ty < width1)
-    {
-        // deciding which quadrature to copy to
-        // for even N - positive f [0, ..., N/2-1] negative f [N/2, ..., N-1]
-        // for odd N - positive f [0, ..., (N-1)/2] negative f [(N+1)/2, ..., N-1]
-        int tx2 = (tx < IDIVUP(height1, 2)) ? tx : height2 - height1 + tx;
-        int ty2 = (ty < IDIVUP(width1, 2)) ? ty : width2 - width1 + ty;
+    //  ensure threads are in the range of image1
+    if (x1 >= height1 || y1 >= width1)
+        return;
 
-        // copy
-        image2[IDX2R(tx2, ty2, width2)+imageIdx*size2]
-            = image1[IDX2R(tx, ty, width1)+imageIdx*size1]*factor;
+    int xcenter1 = (height1+1)/2; // divup
+    int ycenter1 = (width1+1)/2;
+
+    // for even N - positive f[0, ..., N/2-1], f[N/2]/2,
+    //              zeros 0...0,
+    //              negative f[N/2]/2, f[N/2+1, ..., N-1]
+    //        f[N/2] is split to N/2 and M-N/2
+    // for odd N - positive f[0, ..., (N-1)/2],
+    //             zeros 0...0,
+    //             negative f [(N+1)/2, ..., N-1]
+
+    // split spectrum for even height1 at the center
+    if (height1 % 2 ==0 && x1 == xcenter1)
+    {
+        // if also for the width
+        if (width1 % 2 ==0 && y1 == ycenter1)
+        {
+            // split into 4
+            float2 input = image1[IDX2R(x1, y1, width1)+imageIdx*size1];
+            input *= 0.25f*factor;
+            image2[IDX2R(x1, y1, width2)+imageIdx*size2] = input;
+            int x2 = x1 + height2 - height1;
+            int y2 = y1 + width2 - width1;
+            image2[IDX2R(x2, y1, width2)+imageIdx*size2] = input;
+            image2[IDX2R(x1, y2, width2)+imageIdx*size2] = input;
+            image2[IDX2R(x2, y2, width2)+imageIdx*size2] = input;
+            // all done for this pixel, so return
+            return;
+        }
+        // odd width or not at the center
+        float2 input = image1[IDX2R(x1, y1, width1)+imageIdx*size1];
+        input *= 0.5f*factor;
+        int x2 = x1 + height2 - height1;
+        image2[IDX2R(x1, y1, width2)+imageIdx*size2] = input;
+        image2[IDX2R(x2, y1, width2)+imageIdx*size2] = input;
+        return;
     }
+    // split spectrum for even width1
+    if (width1 % 2 ==0 && y1 == ycenter1)
+    {
+        // even height even and at the center has been considered earlier
+        // so just odd height or not at the center
+        float2 input = image1[IDX2R(x1, y1, width1)+imageIdx*size1];
+        input *= 0.5f*factor;
+        int y2 = y1 + width2 - width1;
+        image2[IDX2R(x1, y1, width2)+imageIdx*size2] = input;
+        image2[IDX2R(x1, y2, width2)+imageIdx*size2] = input;
+        return;
+    }
+
+    // all other cases, simply copy
+    // determine the quadrants
+    int x2 = (x1 < xcenter1) ? x1 : height2 - height1 + x1;
+    int y2 = (y1 < ycenter1) ? y1 : width2 - width1 + y1;
+    image2[IDX2R(x2, y2, width2)+imageIdx*size2]
+            = image1[IDX2R(x1, y1, width1)+imageIdx*size1]*factor;
+    return;
 }
 
 /**
