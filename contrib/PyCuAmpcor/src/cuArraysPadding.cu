c656d728470a8a283ba126c6c746c1f39731b528
Lijun Zhu <ljzhu@gps.caltech.edu>
2024-12-22 21:34:31 -0800

    PyCuAmpcor-V2: in zero padding for FFT oversampling, resort to conventional approach to side Nyquist frequency (N/2) with negative frequency

--- contrib/PyCuAmpcor/src/cuArraysPadding.cu
+++ contrib/PyCuAmpcor/src/cuArraysPadding.cu
@@ -11,7 +11,42 @@
 // for both even and odd sequences
 // @param[in] image1 input images
 // @param[inout] image2 output images - memset to 0 in prior
+// @note siding Nyquist frequency for even length with negative frequency
+// for even N - positive f[0, ..., N/2-1],
+//              zeros 0...0,
+//              negative f[N/2], f[N/2+1, ..., N-1]
+// for odd N - positive f[0, ..., (N-1)/2],
+//             zeros 0...0,
+//             negative f [(N+1)/2, ..., N-1]
 __global__ void cuArraysPaddingMany_kernel(
+    const complex_type *image1, const int height1, const int width1, const int size1,
+    complex_type *image2, const int height2, const int width2, const int size2, const real_type factor )
+{
+    // thread indices are for input image1
+    int x1 = threadIdx.x + blockDim.x*blockIdx.x;
+    int y1 = threadIdx.y + blockDim.y*blockIdx.y;
+    int imageIdx =  blockIdx.z;
+
+    //  ensure threads are in the range of image1
+    if (x1 >= height1 || y1 >= width1)
+        return;
+
+    // determine the quadrants
+    // divup the length to be consistent with both even and odd lengths
+    int x2 = (x1 < (height1+1)/2) ? x1 : height2 - height1 + x1;
+    int y2 = (y1 < (width1+1)/2) ? y1 : width2 - width1 + y1;
+    image2[IDX2R(x2, y2, width2)+imageIdx*size2]
+            = image1[IDX2R(x1, y1, width1)+imageIdx*size1]*factor;
+    return;
+}
+
+// cuda kernel for zero padding for FFT oversampling,
+// for both even and odd sequences
+// @param[in] image1 input images
+// @param[inout] image2 output images - memset to 0 in prior
+// @note for even length (height, width or both),
+//       the Nyquist frequency (N/2) component is split between positive and negative frequencies.
+__global__ void cuArraysPaddingMany_split_Nyquist_kernel(
     const complex_type *image1, const int height1, const int width1, const int size1,
     complex_type *image2, const int height2, const int width2, const int size2, const real_type factor )
 {
